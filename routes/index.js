5022b1f9b2da2e14b4445483b2b161106e0ce416
Carl Henderson <chuckhendo@gmail.com>
2016-04-25 10:07:09 -0400

    add api caching

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var packageJson = require("package-json");
 var rp = require("request-promise");
-var _ = require("lodash")
+var _ = require("lodash");
+var moment = require("moment");
 
 var express = require('express');
 var router = express.Router();
@@ -39,34 +40,57 @@ var packagesToCheck = [{
   ghName: "unumux/update-hosts-ip"
 }];
 
+var cache = {
+  packages: [],
+  updated: null
+}
+
+var FIVE_MINUTES = 5 * 60 * 1000;
 
 router.get('/', function(req, res) {
-  var githubPromise = rp({ 
-    uri: "https://api.github.com/users/unumux/repos", 
-    headers: { "User-Agent": "UnumUX" },
-    json: true
-  });
- 
-  var promises = packagesToCheck.map(function(package) {
-    return packageJson(package.name);
-  });
-  
-  promises.push(githubPromise);
-
-  Promise.all(promises).then(function(packages) {
-    var ghData = packages.pop();
-    packages = packages.map(function(package, index) {
-      var ghName = packagesToCheck[index].ghName;
-      package.ghRepo = _.find(ghData, { full_name: ghName });
-      package.cmd = packagesToCheck[index].cmd;
-      
-      return package;  
+  if(cache.updated && ((new Date) - cache.updated) < FIVE_MINUTES) {
+    renderPage(res);
+  } else {
+    updateCache().then(function() {
+      renderPage(res);
     });
-        
-    res.render('index', {
-      packages: packages
-    });
-  });
+  }
 });
+
+function updateCache() {
+  return new Promise(function(resolve, reject) {
+    var githubPromise = rp({ 
+      uri: "https://api.github.com/users/unumux/repos", 
+      headers: { "User-Agent": "UnumUX" },
+      json: true
+    });
+  
+    var promises = packagesToCheck.map(function(package) {
+      return packageJson(package.name);
+    });
+    
+    promises.push(githubPromise);
+
+    Promise.all(promises).then(function(packages) {
+      var ghData = packages.pop();
+      packages = packages.map(function(package, index) {
+        var ghName = packagesToCheck[index].ghName;
+        package.ghRepo = _.find(ghData, { full_name: ghName });
+        package.cmd = packagesToCheck[index].cmd;
+        
+        return package;  
+      });
+      cache.packages = packages;
+      cache.updated = new Date();
+      resolve();
+    });
+  });
+}
+
+function renderPage(res) {
+  res.render('index', {
+    packages: cache.packages
+  });
+}
 
 module.exports = router;
